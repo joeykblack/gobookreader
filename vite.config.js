589aa7367924212b830671e2b942a21099fafce0
joeykblack <joeykblack@gmail.com>
2026-03-27 20:24:56 -0400

    Fix blank page: alias preact to react so Shudan uses React runtime

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: /^preact\/hooks$/, replacement: 'react' },
+      { find: /^preact\/compat$/, replacement: 'react' },
+      { find: /^preact$/, replacement: 'react' },
+    ]
+  },
   build: {
     outDir: 'docs'
   }
